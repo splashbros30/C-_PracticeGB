0833ac0db25619e75c6ea067949e301550fca7a1
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2023-12-24 18:58:45 +0300

    Lesson 1 task 1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter a number a = ");
+int a1_int = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Enter a number b = ");
+int a2_int = Convert.ToInt32(Console.ReadLine()!);
+
+
+if (a2_int == a1_int*a1_int)
+{
+    Console.Write("b - квадрат числа a");
+}
+else{
+    Console.Write("b - не является квадратом числа a");
+}
