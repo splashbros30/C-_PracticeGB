83b1f816b8ee6efd1e0dd0ed389b806b8836a373
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2023-12-24 19:30:09 +0300

    Lesson 1 task 2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter you number : ");
+int a = int.Parse(Console.ReadLine()!);
+int i = -a;
+
+if (a >= 0)
+{
+while (i <= a)
+{
+    Console.Write(i + " ");
+    i = i + 1;
+}
+}
+else 
+{
+    while (i >= a)
+    {
+        Console.Write(i + " ");
+        i = i - 1;
+    }
+}
