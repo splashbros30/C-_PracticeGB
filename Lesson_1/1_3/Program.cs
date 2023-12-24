c3ace98e96d4005b38e88b3b0bf9adc4f7d060f7
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2023-12-24 19:41:17 +0300

    Lesson 1 task 3*

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter you number : ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a%10;
+int c = a / 100;
+int d = b + c;
+if (a > 0)
+{
+Console.WriteLine("Summ of first and third numbers of entered number = " + d);
+}
+else{
+    Console.WriteLine("Summ of first and third numbers of entered number = " + (-d));
+}
