a37eb212ffe7a25e1c0ebc47fa1006abdebac101
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-01-14 17:40:36 +0300

    Создал папку ДЗ и добавил в неё решение первого задания

---
+++ Homework_from_11_01_2024/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Задайте одномерный массив из 10 целых
+// чисел от 1 до 100. Найдите количество элементов
+// массива, значения которых лежат в отрезке [20,90]. 
+
+int[] arr  = new int[10];
+Random rand = new();
+Console.Write("Массив: [ ");
+for (int i = 0; i < arr.Length; i++)
+{
+   arr[i] = rand.Next(1, 100);
+   Console.Write($"{arr[i]}   ");
+}
+Console.WriteLine("]");
+Console.WriteLine();
+
+int count = 0;
+
+for (int j = 0; j < arr.Length; j++)
+{
+    if ((arr[j] >= 20) && (arr[j] <= 90) )
+    {
+        count++;
+    }
+}
+
+Console.Write($"Количество чисел в массиве из промежутка [20, 90] = {count}");
