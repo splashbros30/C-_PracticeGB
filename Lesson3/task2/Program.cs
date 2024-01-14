c50167162dd9966d1f6876490bc0a39e1a8b7ed5
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-01-14 12:16:19 +0300

    Добавил 3 урок и 3 задания из него

---
+++ Lesson3/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+
+int N = 10;
+int[] arr = new int[N];
+int i = 0;
+
+Console.WriteLine($"Введите все {N} элементов(-а) массива из промежутка [-10;10]: ");
+
+while (i < arr.Length)
+{
+    arr[i] = int.Parse(Console.ReadLine()!);
+    if ((-10 > arr[i]) || (10 < arr[i]))
+    {
+        Console.WriteLine("WARNING!!!" + '\n' + "Необходимо вводить числа только из промежутка [-10;10]:");
+        break;
+    }
+    i++;
+}
+
+for (int j = 0; j < arr.Length; j++)
+{
+    arr[j] = arr[j] * (-1);
+}
+
+Console.WriteLine($"Массив после замены : ");
+
+for (int e = 0; e < arr.Length; e++)
+{
+    Console.Write($"{arr[e]} ");
+}
