666693b955d9363c5f3bc5938e3f709231544542
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-01-23 12:16:35 +0300

    Добавил решение ко второму заданию

---
+++ Homework_15_01_2024/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(99, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+int CalculateEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество элементов массива: ");
+int a = int.Parse(Console.ReadLine()!);
+int[] arr = CreateArray(a);
+PrintArray(arr);
+Console.Write(" => ");
+Console.WriteLine(CalculateEvenNumbers(arr));
