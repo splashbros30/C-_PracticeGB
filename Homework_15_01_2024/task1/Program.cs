bf9ebaf7f8bca959bf4d7f2601240867c79b8d2b
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-01-23 12:03:04 +0300

    Добавил решение к 1 задаче из дз

---
+++ Homework_15_01_2024/task1/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые 
+// числа с консоли. Программа завершается при вводе символа ‘q’ или 
+// при вводе числа, сумма цифр которого чётная.
+
+int SummNumbers(int num)
+{
+    int b = 0;
+    int count = 0;
+    while (num != 0)
+    {
+        b = b + (num % 10);
+        num = num / 10;
+    }
+
+    Console.WriteLine(b);
+
+    if (b % 2 == 0)
+    {
+        count = 1;
+    }
+    else
+    {
+        count = 0;
+    }
+    return count;
+}
+
+int StopMethod(int c)
+{
+    int ans = 0;
+    if (c == 1)
+    {
+        ans = 1;
+    }
+    return ans;
+}
+
+for (; ; )
+{
+    Console.WriteLine("Введите целое число или завершающую 'q' : ");
+    string a = Console.ReadLine()!;
+
+    int x;
+
+    if (int.TryParse(a, out x))
+    {
+        if (StopMethod(SummNumbers(x)) == 1)
+        {
+            break;
+        }
+    }
+    else if (a == "q")
+    {
+            break;
+    }
+
+    continue;
+}
