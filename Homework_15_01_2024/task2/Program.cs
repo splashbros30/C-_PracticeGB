99a8f05152b62134d986c840c24b25f8e616995a
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-01-23 12:37:57 +0300

    Исправил условие во 2 заднии из дз

--- Homework_15_01_2024/task2/Program.cs
+++ Homework_15_01_2024/task2/Program.cs
@@ -6,7 +6,7 @@ int[] CreateArray(int size)
     int[] arr = new int[size];
     for (int i = 0; i < size; i++)
     {
-        arr[i] = new Random().Next(99, 1000);
+        arr[i] = new Random().Next(100, 1000);
     }
     return arr;
 }
