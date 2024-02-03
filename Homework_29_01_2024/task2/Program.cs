e9b9fe6aca56423dbf37976456094a2fddbc8be0
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-02-03 18:36:43 +0300

    Добавил решение 2 задания из дз

---
+++ Homework_29_01_2024/task2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. 
+
+using System;
+
+
+
+class prog
+{
+    static Random rand = new Random();
+    public static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Введите строку:");
+        string str = Console.ReadLine()!;
+        char[] characters = str.ToCharArray();
+        Console.WriteLine();
+        Console.WriteLine("Строка после изменения:");
+        Console.WriteLine(char_to_string(is_upper_or_is_lower(characters)));
+    }
+
+    public static char[] is_upper_or_is_lower(char[] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            if (Char.IsUpper(array[i]))
+            {
+                array[i] = Char.ToLower(array[i]);
+            }
+        }
+        return array;
+    }
+
+    public static string char_to_string(char[] array)
+    {
+        string temp = "";
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            temp = temp + array[i];
+        }
+        return temp;
+    }
+}
