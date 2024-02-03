7aafd782d33f3ffd0ef7e9fb4811e8c872269551
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-02-03 19:59:09 +0300

    Добавил решение к 4 задаче из дз

---
+++ Homework_29_01_2024/task4/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+
+
+using System;
+
+
+
+class prog
+{
+    static Random rand = new Random();
+    public static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Введите строку:");
+        string str = Console.ReadLine()!;
+        char[] characters = str.ToCharArray();
+        Console.WriteLine();
+        Console.WriteLine("Строка со словами наоборот:");
+        Console.WriteLine(result(characters));
+    }
+
+    public static string result(char[] array)
+    {
+        string temp = "";
+        string sp = " ";
+        char[] space = sp.ToCharArray();
+        string t = "";
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            if (array[i] == space[0])
+            {
+                temp = t + " " + temp;
+                t = "";
+            }
+            else
+            {
+                t = t + array[i];
+                if (array[i] == array[array.GetLength(0) - 1])
+                {
+                    temp = t + " " + temp;
+                }
+            }
+        }
+        return temp;
+    }
+
+
+}
