d1127e812e5508cc9d35d9a93a0ef5e3433c1c68
Denis Nikolaev <den_nikolaev_2000@mail.ru>
2024-02-07 12:00:43 +0300

    Добавил дебаг файл, добавил папку с дз для 7 семинара, добавил тестовую папку

---
+++ Homework_01_02_2024/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. 
+// !Использовать рекурсию, не!
+// !использовать циклы.!
+
+using System;
+
+class Prog
+{
+    public static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Enter a number from: ");
+        int M = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enter a number to: ");
+        int N = int.Parse(Console.ReadLine()!);
+    }
+
+    public static void range_from_numbers(int m, int n)
+    {
+        int i = m;
+        if (i == n)
+        {
+            return;
+        }
+        Console.Write($"");
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,10 @@
+﻿int fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+
+Console.WriteLine(fact(5));
